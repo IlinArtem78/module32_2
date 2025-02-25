f140b1d01a7646c718f9d1e2522a19162479ffb2
IlinArtem78 <artem.ilin1994@gmail.com>
2025-02-25 14:45:19 +0300

    ver1

---
+++ MVC/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace WebApplication1
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        ///  Middleware-компонент должен иметь конструктор, принимающий RequestDelegate
+        /// </summary>
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        ///  Необходимо реализовать метод Invoke  или InvokeAsync
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Для логирования данных о запросе используем свойста объекта HttpContext
+            Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+
+            string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+
+            // Путь до лога (опять-таки, используем абсолютный путь либо Directory.GetCurrentDirectory())
+            string logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "RequestLog.txt");
+
+            // Используем асинхронную запись в файл
+            await File.AppendAllTextAsync(logFilePath, logMessage);
+
+            // Передача запроса далее по конвейеру
+            await _next.Invoke(context);
+        }
+    }
+}
